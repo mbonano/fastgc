c9052f6ced22338381df8ab222793634fbee494b
Mark Bonano <mbonano@gmail.com>
2026-05-28 09:51:31 -0400

    Added google analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
 import Providers from './components/Providers'
 import Header from './components/Header'
@@ -23,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Footer />
         </Providers>
       </body>
+      <GoogleAnalytics gaId="G-SJRZ0FFKQ8" />
     </html>
   )
 }
